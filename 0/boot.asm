4ef8fee62ba703eb22e910b2af934ef0bee58cc2
jweir136 <31812897+jweir136@users.noreply.github.com>
2024-01-02 16:57:59 -0500

    init bootloader branch

--- 0/boot.asm
+++
@@ -1,9 +0,0 @@
-; this is the most simple boot sector. It just goes in an infinite loop upon boot
-loop:
-	jmp loop
-
-; pad the file with 0 bytes so that is is length 510 (saving last 2 bytes for magic number)
-times 510-($-$$) db 0
-
-; pad the file with the bytes 0xaa55 (the magic number for a boot sector)
-dw 0xaa55
